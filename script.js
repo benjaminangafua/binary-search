32b930adc57bf0cea86af9268d35522734a015ee
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-06-05 23:20:00 +0000

    Binary search recent update

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
- let hundredNumerals = [
-     91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
-     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-     61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-     41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-     71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-     51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-     81, 82, 83, 84, 85, 86, 87, 88, 89, 90
- ]
- let sortedNum = hundredNumerals.sort((a, b) => { return a - b });
+/**
+ * Binary search
+ * Search a number from the array
+ * *Check in the middle 
+ * *if not the right 
+ * *if not in the left
+ * Else return not found
+ **/
+let hundredNumerals = [
+    91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90
+]
+let sortedNum = hundredNumerals.sort((a, b) => { return a - b });
+document.querySelector("#arrayNum").insertAdjacentHTML("beforeend", `${sortedNum}`);
 
- function findNum() {
-     const SEARCHINGNUMBER = Number(document.querySelector("#num").value);
-     searchNum(SEARCHINGNUMBER, sortedNum)
- }
+function searchNumber(arr, l, r, search) {
 
+    if (r >= l) {
+        let middle = l + Math.round((r - l) / 2);
 
- function searchNum(num, arr) {
+        if (arr[middle] == search) {
+            return middle
+        }
+        if (arr[middle] > search) {
+            return searchNumber(arr, l, middle - 1, search)
+        }
+        return searchNumber(arr, middle + 1, r, search)
+    }
+    return alert("Enter a valid number")
+}
 
-     let halfTheNum = Math.round(arr.length / 2);
+document.querySelector("#clk-btn").addEventListener("click", () => {
+    console.log("CLick");
+    const SEARCHINGNUMBER = Number(document.querySelector("#num").value);
 
-     if (num === arr[middle]) {
-         return num
-     } else {
-         if (num < arr[middle]) {
-             arr.splice(middle);
-             console.log("This is", arr)
+    let arra = sortedNum;
+    let right = arra.length;
+    let result = searchNumber(arra, 0, right - 1, SEARCHINGNUMBER);
 
-         } else if (num > arr[middle]) {
-             arr.splice(0, middle)
-             console.log(arr)
-         }
-         searchNum(num, arr)
-     }
+    document.querySelector("#searchPos").insertAdjacentHTML("beforebegin",
+        `Indext of the number is: ${result}`)
 
- }
+})
